1cb96cb066b03434be96807e552f349321b19348
robSamuel <robert_samuel97@hotmail.com>
2025-08-28 04:31:47 -0600

    feat: add the tests for the atoms

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,13 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
+	},
+	test: {
+		environment: 'jsdom',
+		setupFiles: ['./vitest.setup.ts'],
+		globals: true,
+		css: true,
+		include: ['src/**/*.{test,spec}.{ts,tsx}'],
+		exclude: [...configDefaults.exclude, 'node_modules/**'],
 	},
 });
